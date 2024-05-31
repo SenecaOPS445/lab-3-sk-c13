974d55858d87e87f606123178dbb654da41a79a7
Sagar K C <sk-c13@myseneca.ca>
2024-05-31 13:51:22 -0400

    Added lab3a.py

---
+++ lab3a.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+# return_text_value() function
+# Author ID: [your_seneca_id]
+
+def return_text_value():
+    return "Good Morning Terry"
+
+
+
+def return_number_value():
+    a = 10
+    b = 5
+    return a + b
+
+
+
+if __name__ == '__main__':
+    print('python code')
+    text = return_text_value()
+    print(text)
+    number = return_number_value()
+    print(str(number))
